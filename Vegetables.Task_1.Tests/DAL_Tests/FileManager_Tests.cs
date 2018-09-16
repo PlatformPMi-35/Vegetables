a442335bae6be0daa877e23831ffaa38b71b66cf
Nazar Homeniuk <gomenyuknazar@gmail.com>
2018-09-16 11:55:54 +0300

    Added FileManager tests

---
+++ Vegetables.Task_1.Tests/DAL_Tests/FileManager_Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using Vegetables.Task_1.DAL.Managers;
+
+namespace Vegetables.Task_1.Tests.DAL_Tests
+{
+    [TestFixture]
+    public class FileManager_Tests
+    {
+        private string _path;
+        private FileManager _fileManager;
+
+        [SetUp]
+        public void Setup()
+        {
+            File.Create(Directory.GetCurrentDirectory() + "\\Test.csv");
+            _fileManager = new FileManager();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            File.Delete(Directory.GetCurrentDirectory() + "\\Test.csv");
+        }
+
+        [Test]
+        public void LoadWithNullPath_Test()
+        {
+            //Arrange
+            //Act
+            Assert.Throws<ArgumentNullException>(() => _fileManager.Load(null));
+        }
+    }
+}
